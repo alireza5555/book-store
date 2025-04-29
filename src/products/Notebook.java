d823d9dd3432414238ae25a5c0715d453f4b0d62
Alireza <rostmi83@gmail.com>
2025-04-29 21:17:29 +0330

    add fields and methods to notebook class

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -1,4 +1,50 @@
 package products;
 
-public class Notebook {
+import javax.swing.plaf.ProgressBarUI;
+
+public class Notebook extends Product {
+    private static final int NOTE_BOOK = 2000;
+
+    private int pageCount;
+    private boolean isHardCover;
+
+    public Notebook(String title, Double price, int pageCount, boolean isHardCover) {
+        super(title, price);
+        this.isHardCover = isHardCover;
+        this.pageCount = pageCount;
+        setId(generateId());
+
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public boolean isHardCover() {
+        return isHardCover;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public void setHardCover(boolean hardCover) {
+        isHardCover = hardCover;
+    }
+
+    @Override
+    public String toString (){
+        String temp = null;
+        if(isHardCover) temp = "Hard";
+        else temp = "Soft";
+        return super.toString() + "\nPage Count: " + pageCount + "\nPage Cover is " + temp;
+    }
+
+    @Override
+    protected String generateId() {
+        String temp = String.valueOf(NOTE_BOOK + num);
+        num++;
+        return temp;
+
+    }
 }
